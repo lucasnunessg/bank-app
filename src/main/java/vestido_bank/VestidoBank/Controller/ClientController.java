16a09cac4be0e59a2c953a7b94c5ac5b72ac395c
lucasnunessg <lucasnunespacheco@gmail.com>
2024-12-10 16:04:46 -0300

    criando dtos e controllers

--- src/main/java/vestido_bank/VestidoBank/Controller/ClientController.java
+++ src/main/java/vestido_bank/VestidoBank/Controller/ClientController.java
@@ -2,10 +2,15 @@ package vestido_bank.VestidoBank.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vestido_bank.VestidoBank.Controller.Dto.ClientDto;
+import vestido_bank.VestidoBank.Controller.Dto.CreateClientDto;
 import vestido_bank.VestidoBank.Entity.Client;
+import vestido_bank.VestidoBank.Exceptions.ClientNotFoundException;
 import vestido_bank.VestidoBank.Service.ClientService;
 import java.util.List;
 
@@ -25,6 +30,17 @@ public class ClientController {
     List<Client> clients = clientService.getAllClients();
     return clients.stream().map(ClientDto::fromEntity)
         .toList();
+  }
+
+  @GetMapping("/{id}")
+  public ClientDto getClientByid(@PathVariable Long id) {
+    return ClientDto.fromEntity(clientService.getById(id));
+  }
+
+  @PostMapping("/{id}")
+  public ClientDto createClient(@RequestBody CreateClientDto createClientDto)
+      throws ClientNotFoundException {
+    return ClientDto.fromEntity(clientService.createClient(createClientDto.toEntity()));
   }
 
 }
